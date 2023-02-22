6b3d95eb0d38fcd767aadaa1ab49808da7987c47
Karl <karlmeisner99@gmail.com>
2023-02-22 11:19:16 +0100

    add dqn_agent, change name of sumo-pp

---
+++ sumo/sumo_normal_agent.py
@@ -0,0 +1,282 @@
+import numpy as np
+import torch
+from sumo import sumo_pp
+
+import os
+from typing import Dict, List, Tuple
+import gym
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from IPython.display import clear_output
+
+class SumoNormalAgent:
+    def __init__(self):
+        self.replay_buffer = {}
+
+
+
+class Network(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int):
+        """Initialization."""
+        super(Network, self).__init__()
+
+        self.layers = nn.Sequential(
+            nn.Linear(in_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, out_dim)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward method implementation."""
+        return self.layers(x)
+
+class RegularDQN:
+    def __init__(self, replay_size, state_dim, action_dim, batch_size, env,target_update, epsilon_decay, max_epsilon=1.0, min_epsilon=0.1, gamma=0.99):
+        self.state_buffer = np.zeros([replay_size, state_dim], dtype=np.float32)
+        self.next_state_buffer = np.zeros([replay_size, state_dim], dtype=np.float32)
+        self.action_buffer = np.zeros([replay_size], dtype=np.float32)
+        self.reward_buffer = np.zeros([replay_size], dtype=np.float32)
+        self.done_buffer = np.zeros(replay_size, dtype=np.float32)
+        self.max_size, self.batch_size = replay_size, batch_size
+        self.pointer, self.size = 0,0
+
+        self.env = env
+        self.target_update = target_update # How
+        self.epsilon_decay = epsilon_decay
+        self.epsilon=max_epsilon
+        self.max_epsilon = max_epsilon
+        self.min_epsilon = min_epsilon
+        self.gamma = gamma
+
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )
+
+        self.dqn = Network(state_dim, action_dim).to(self.device)
+        self.dqn_target = Network(state_dim, action_dim).to(self.device)
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+        self.dqn_target.eval()
+
+        self.optimizer = optim.Adam(self.dqn.parameters())
+
+        # transition to store in memory
+        self.transition = list()
+
+        # mode: train / test
+        self.is_test = False
+
+
+    def store(self, state, action, reward, next_state, done):
+        self.state_buffer[self.pointer] = state
+        self.next_state_buffer[self.pointer] = next_state
+        self.action_buffer[self.pointer] = action
+        self.reward_buffer[self.pointer] = reward
+        self.done_buffer[self.pointer] = done
+        self.pointer = (self.pointer + 1) % self.max_size # Makes the pointer wrap around when max size is reached
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample_batch(self) -> Dict[str, np.ndarray]:
+        idxs = np.random.choice(self.size, size=self.batch_size, replace=False)
+        return dict(obs=self.state_buffer[idxs],
+                    next_obs=self.next_state_buffer[idxs],
+                    acts=self.action_buffer[idxs],
+                    rews=self.reward_buffer[idxs],
+                    done=self.done_buffer[idxs])
+
+    def __len__(self) -> int:
+        return self.size
+
+    def select_action(self, state: np.ndarray) -> np.ndarray:
+        """Select an action from the input state."""
+        # epsilon greedy policy
+        if self.epsilon > np.random.random():
+            selected_action = self.env.action_space.sample()
+        else:
+            selected_action = self.dqn(
+                torch.FloatTensor(state).to(self.device)
+            ).argmax()
+            selected_action = selected_action.detach().cpu().numpy()
+
+        if not self.is_test:
+            self.transition = [state, selected_action]
+
+        return selected_action
+
+    def step(self, action: np.ndarray) -> Tuple[np.ndarray, np.float64, bool]:
+        """Take an action and return the response of the env."""
+        next_state, reward, done, _ = self.env.step(action)
+
+        if not self.is_test:
+            self.transition += [reward, next_state, done]
+            self.store(*self.transition)
+
+        return next_state, reward, done
+
+    def update_model(self) -> torch.Tensor:
+        """Update the model by gradient descent."""
+        samples = self.sample_batch()
+
+        loss = self._compute_dqn_loss(samples)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return loss.item()
+
+    def train(self, num_frames: int, plotting_interval: int = 200):
+        """Train the agent."""
+        self.is_test = False
+
+        state = self.env.reset()
+        update_cnt = 0
+        epsilons = []
+        losses = []
+        scores = []
+        score = 0
+
+        for frame_idx in range(1, num_frames + 1):
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+            # if episode ends
+            if done:
+                state = self.env.reset()
+                scores.append(score)
+                score = 0
+
+            # if training is ready
+            if len(self) >= self.batch_size:
+                loss = self.update_model()
+                losses.append(loss)
+                update_cnt += 1
+
+                # linearly decrease epsilon
+                self.epsilon = max(
+                    self.min_epsilon, self.epsilon - (
+                            self.max_epsilon - self.min_epsilon
+                    ) * self.epsilon_decay
+                )
+                epsilons.append(self.epsilon)
+
+                # if hard update is needed
+                if update_cnt % self.target_update == 0:
+                    self._target_hard_update()
+
+            # plotting
+            if frame_idx % plotting_interval == 0:
+                self._plot(frame_idx, scores, losses, epsilons)
+
+        self.env.close()
+
+    def test(self, video_folder: str) -> None:
+        """Test the agent."""
+        self.is_test = True
+
+        # for recording a video
+        naive_env = self.env
+        self.env = gym.wrappers.RecordVideo(self.env, video_folder=video_folder)
+
+        state = self.env.reset()
+        done = False
+        score = 0
+
+        while not done:
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+        print("score: ", score)
+        self.env.close()
+
+        # reset
+        self.env = naive_env
+
+    def _compute_dqn_loss(self, samples: Dict[str, np.ndarray]) -> torch.Tensor:
+        """Return dqn loss."""
+        device = self.device  # for shortening the following lines
+        state = torch.FloatTensor(samples["obs"]).to(device)
+        next_state = torch.FloatTensor(samples["next_obs"]).to(device)
+        action = torch.LongTensor(samples["acts"].reshape(-1, 1)).to(device)
+        reward = torch.FloatTensor(samples["rews"].reshape(-1, 1)).to(device)
+        done = torch.FloatTensor(samples["done"].reshape(-1, 1)).to(device)
+
+        # G_t   = r + gamma * v(s_{t+1})  if state != Terminal
+        #       = r                       otherwise
+        curr_q_value = self.dqn(state).gather(1, action)
+        next_q_value = self.dqn_target(
+            next_state
+        ).max(dim=1, keepdim=True)[0].detach()
+        mask = 1 - done
+        target = (reward + self.gamma * next_q_value * mask).to(self.device)
+
+        # calculate dqn loss
+        loss = F.smooth_l1_loss(curr_q_value, target)
+
+        return loss
+
+    def _target_hard_update(self):
+        """Hard update: target <- local."""
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+
+    def _plot(
+            self,
+            frame_idx: int,
+            scores: List[float],
+            losses: List[float],
+            epsilons: List[float],
+    ):
+        """Plot the training progresses."""
+        clear_output(True)
+        plt.figure(figsize=(20, 5))
+        plt.subplot(131)
+        plt.title('frame %s. score: %s' % (frame_idx, np.mean(scores[-10:])))
+        plt.plot(scores)
+        plt.subplot(132)
+        plt.title('loss')
+        plt.plot(losses)
+        plt.subplot(133)
+        plt.title('epsilons')
+        plt.plot(epsilons)
+        plt.show()
+
+
+
+
+# environment
+env_id = "CartPole-v0"
+env = gym.make(env_id)
+
+seed = 777
+
+def seed_torch(seed):
+    torch.manual_seed(seed)
+    if torch.backends.cudnn.enabled:
+        torch.backends.cudnn.benchmark = False
+        torch.backends.cudnn.deterministic = True
+
+np.random.seed(seed)
+seed_torch(seed)
+env.seed(seed)
+
+# parameters
+num_frames = 10000
+memory_size = 1000
+batch_size = 32
+target_update = 100
+epsilon_decay = 1 / 2000
+
+agent = RegularDQN(env, memory_size, batch_size, target_update, epsilon_decay)
+
+agent.train(num_frames)
