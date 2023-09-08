5826783646bb8265abe391aef416766b4c069c28
CalleRC <calle@rygecarlsen.dk>
2023-09-08 09:32:03 +0200

    new branch

---
+++ post_bachelor/cliff_car/cliff_car_env.py
@@ -0,0 +1,252 @@
+import numpy as np
+import pygame
+import matplotlib.pyplot as plt
+import os
+from collections import defaultdict
+
+def drawImage(display, path, center, scale=None, angle=None):
+    img = pygame.image.load(path)
+    if (scale != None):
+        img = pygame.transform.scale(img, scale)
+    if (angle != None):
+        img = pygame.transform.rotate(img, angle)
+
+    display.blit(img, center)
+
+class CliffCar:
+
+    ACTIONS = [np.array([0,0]),np.array([1,0]),np.array([0,1]),np.array([-1,0]),np.array([0,-1])]
+    CLIFF_PENALTY = -1
+    ACTION_DIM = len(ACTIONS)
+    OBS_DIM = 2
+    START_POSITION = np.array([1,1])
+    GOAL_POSITION = np.array([14,1])
+    BOUNDS = np.array([-5,-5,20,15])
+    CLIFF_HEIGHT = 0
+    SPEED = 0.5
+
+    def __init__(self, noise_mean = np.array([0,0]), noise_var = None, mode = "abrupt"):
+
+        # Car settings
+        self.position = self.START_POSITION
+
+        self.noise_mean = noise_mean
+        self.noise_var = noise_var
+        
+        # World settings
+        self.mode = mode # "abrupt" or "penalty" - How the cliff is handled
+        self.max_goal_distance = self.get_max_goal_distance()
+        
+        # Simulation settings
+        self.max_duration = 1000
+        self.frame = 0
+        
+    def clamp_position(self, position):
+        position[0] = min(max(position[0], self.BOUNDS[0]), self.BOUNDS[2])
+        position[1] = min(max(position[1], self.BOUNDS[1]), self.BOUNDS[3])
+        
+        return position
+    
+    def get_max_goal_distance(self):
+        d1 = np.sum((self.GOAL_POSITION - self.BOUNDS[0:2])**2)
+        d2 = np.sum((self.GOAL_POSITION - self.BOUNDS[2:4])**2)
+        d3 = np.sum((self.GOAL_POSITION - self.BOUNDS[[0,3]])**2)
+        d4 = np.sum((self.GOAL_POSITION - self.BOUNDS[[2,1]])**2)
+        return max(d1,d2,d3,d4)
+    
+    def step(self, action):
+        
+        self.position = self.result(self.position, action)
+
+        reward, done = self.reward_fn(self.position)
+        
+
+        if self.frame >= self.max_duration:
+            done = True
+            reward = 0
+
+        if self.rendering:
+            self.render()
+            
+        self.frame += 1
+
+        return self.position, reward, done, 'dummy' # Final value is dummy for working with gym envs
+    
+    def result(self, position, action):
+        noise = np.zeros(2)
+        if(self.noise_var is not None):
+            noise = np.random.multivariate_normal(self.noise_mean, self.noise_var)
+
+        # Calculate the new position and clamp it
+        position = position + self.SPEED * action + noise
+        position = self.clamp_position(position)
+        
+        return position
+
+    def reward_fn(self, position):
+        
+        # If the car is below the cliff 
+        if position[1] < self.CLIFF_HEIGHT:
+            if self.mode == "abrupt":
+                # End the game immediatly
+                return 0, True
+            elif self.mode == "penalty":
+                # Give a penalty for being in the cliff. Don't end the game
+                return self.CLIFF_PENALTY, False
+            else: raise ValueError("Invalid mode. Should either be 'abrupt' or 'penalty'")
+        
+        # If the car is above the cliff (else)
+        
+        # Calculate distance to cliff (as reward)
+        reward = (position[0] - self.GOAL_POSITION[0])**2 + (position[1] - self.GOAL_POSITION[1])**2
+        reward = 1 - reward/self.max_goal_distance # Normalize distance
+        
+        return reward, False
+    
+    def reset(self):
+        self.position = self.START_POSITION
+        self.frame = 0 # Reset the timer (pretty important)
+        return self.position
+
+    def init_render(self, scale = 25):
+        self.rendering = True
+        self.scale = scale
+        self.clock = pygame.time.Clock()
+        self.render_width = abs(self.BOUNDS[2] - self.BOUNDS[0]) * scale
+        self.render_height = abs(self.BOUNDS[3] - self.BOUNDS[1]) * scale
+        self.display = pygame.display.set_mode((self.render_width, self.render_height))
+        
+        self.show_grid = False # Show grid lines (scale): keycode g
+
+    def render(self):
+
+        def tfx(x):
+            return (abs(self.BOUNDS[0]) + x) * self.scale
+
+        def tfy(y):
+            return (abs(self.BOUNDS[1]) + y) * self.scale
+
+        # Transform the coordinates of the game logic to the coordinates of the display
+        def tf2(coord):
+            x = tfx(coord[0])
+            y = tfy(coord[1])
+            return (x, y)
+        
+        def flip_rect(rect):
+            return (rect[0], self.render_height - rect[1] - rect[3], rect[2], rect[3])
+
+        if not self.rendering:
+            self.init_render()
+
+
+        self.clock.tick(60) # frame_rate = 60
+        pygame.display.flip() # Why this?
+        self.display.fill((255,255,255)) # Fill with background color - Do before other stuff
+        
+        # The cliff
+        
+        cliff_rect = flip_rect((0, 0, self.render_width, tfy(self.CLIFF_HEIGHT)))
+        tf_goal = tf2(self.GOAL_POSITION)
+        tf_car = tf2(self.position)
+        goal_rect = flip_rect((tf_goal[0] - 10, tf_goal[1] - 10, 20, 20))
+        car_rect = flip_rect((tf_car[0] - 20, tf_car[1] - 20, 40, 40))
+        
+        pygame.draw.rect(self.display, (0, 0, 255), cliff_rect)
+        
+        if(self.show_grid):
+            for x in range(0, self.render_width, self.scale):
+                pygame.draw.line(self.display, (20,20,20), (x, 0), (x, self.render_height))
+            for y in range(0, self.render_height, self.scale):
+                pygame.draw.line(self.display, (20,20,20), (0, y), (self.render_width, y))
+        
+        pygame.draw.rect(self.display, (0,255,0), goal_rect)
+        pygame.draw.rect(self.display, (255,0,0), car_rect)
+        
+        
+class CliffCarDiscrete(CliffCar):
+    
+    # These needs to be defined here since the values in CliffCar may be continous
+    START_POSITION = np.array([1,1])
+    GOAL_POSITION = np.array([14,1])
+    BOUNDS = np.array([-3,-2,18,12])
+    CLIFF_HEIGHT = 0
+    
+    def __init__(self, noise_mean = np.array([0,0]), noise_var = None, mode = "abrupt"):
+        super().__init__(noise_mean, noise_var, mode)
+    
+    def result(self, position, action):
+        position = super().result(position, action)
+        
+        # Round to nearest integer
+        position = np.round(position,0)
+        
+        return position
+    
+    def get_states(self):
+        states = []
+        for x in range(self.BOUNDS[0], self.BOUNDS[2]+1):
+            for y in range(self.BOUNDS[1], self.BOUNDS[3]+1):
+                states.append(np.array([x,y]))
+        return states
+        
+    def transition_prob(self, position, action, sim = 50):
+        # Returns a dictionary of probabilities where the keys are (s', reward)
+        
+        p = defaultdict(float)
+        
+        for i in range(sim):
+            next_position = self.result(position, action)
+            reward = self.reward_fn(next_position)[0]
+            p[tuple(next_position), reward] += 1/sim
+        
+        return p
+
+        
+        
+
+car = CliffCar()
+
+if __name__ == "__main__":
+    env = CliffCarDiscrete(noise_var = np.array([[0.5,0],[0,0.5]]))
+    env.init_render()
+
+    action = 0
+    done = False
+    episode_reward = 0
+    while True:
+
+        while not done:
+            
+            action_taken = False
+            
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
+                        pygame.quit()
+                        quit()
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        action = np.array([-1,0])
+                    elif event.key == pygame.K_d:
+                        action = np.array([1,0])
+                    elif event.key == pygame.K_w:
+                        action = np.array([0,1])
+                    elif event.key == pygame.K_s:
+                        action = np.array([0,-1])
+                    elif event.key == pygame.K_g:
+                        env.show_grid = not env.show_grid
+                        
+                    action_taken = True
+            
+            if action_taken:
+                print(env.reward_fn(env.position)[0])
+                next_state, reward, done, _ = env.step(action)
+
+                episode_reward += reward
+            env.render()
+            
+        done = False
+        state = env.reset()
+        print(episode_reward)
+
---
+++ sum_plot.py
@@ -0,0 +1,220 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from sumo.sumo_agent import SumoAgent
+from sumo.sumo_pp import SumoPPEnv
+import torch
+import sys
+
+from cliff_car.cliff_car_again import CliffCar
+from cliff_car.cliff_car_agent import CliffCarAgent
+
+# states = torch.FloatTensor(np.linspace(0, 1000, 5000)).reshape(-1, 1).to('cuda')
+# print(states)
+# Load the .npy file
+
+root_folder = 'sumo/test_results/test_seed_420_robust_factor_-1'
+root_folder = 'sumo/test_results/Truelinear-test_seed_22_robust_factor_-1'
+root_folder = 'sumo/test_results/newoptim-linear-True-test_seed_5318008_robust_factor_-1'
+sys.path.append(os.getcwd() + '/sumo')
+sys.path.append('cliff_car')
+
+def get_and_plot_csv_data(delta=None, path=None):
+    if delta is not None:
+        dat = pd.read_csv(os.path.join(root_folder, str(delta))+'-train_data.csv')
+        score_dat = pd.read_csv(os.path.join(root_folder, str(delta)) + '-train_score_data.csv')
+
+    elif path is not None:
+        dat = pd.read_csv(path + '-train_data.csv')
+        score_dat = pd.read_csv(path + '-train_score_data.csv')
+
+    else:
+        raise NotImplemented
+
+    plt.figure(figsize=(15, 5))
+    plt.subplot(131)
+    plt.plot(score_dat['scores'])
+    plt.title(f'Scores, Delta: {delta}')
+    plt.subplot(132)
+    plt.plot(dat['losses'])
+    plt.title(f'Losses, Delta: {delta}')
+    plt.subplot(133)
+    plt.plot(dat['epsilons'])
+    plt.title(f'Epsilons, Delta: {delta}')
+    # plt.tight_layout()
+    plt.show()
+
+def get_q_vals(delta=None, path=None):
+    if delta is not None:
+        q_vals = np.load(os.path.join(root_folder, str(delta)) + '-q_vals.npy')
+
+    elif path is not None:
+        q_vals = np.load(path)
+        # q_vals = np.load(path + '-q_vals.npy')
+
+    else:
+        raise NotImplemented
+
+    return q_vals
+
+
+def plot_q_vals(q_vals, delta=None, same_plot=True, vertical_lines=False, linear=None, save_folder=None):
+
+
+    column1 = q_vals[:, 0]
+    column2 = q_vals[:, 1]
+    column3 = q_vals[:, 2]
+
+    if not same_plot:
+        plt.figure(figsize=(15, 5))
+        plt.subplot(131)
+        plt.plot(column1)
+        plt.title(f'Action 0, {delta}')
+        plt.subplot(132)
+        plt.plot(column2)
+        plt.title('Action 1')
+        plt.subplot(133)
+        plt.plot(column3)
+        plt.title('Action 2')
+        plt.tight_layout()
+    else:
+        plt.plot(column1, color='red', label='0: Noop')
+        plt.plot(column2, color='blue', label='1: Right (towards cliff)')
+        plt.plot(column3, color='green', label='2: Left (away from cliff)')
+        plt.legend()
+        plt.title(f'Q-values for converged agent with Delta {delta} with linear={linear}')
+
+    if vertical_lines:
+        # Add lines to indicate where right becomes better action than left
+        indices = np.where(column3 > column2)[0]
+        indices1 = np.where(column1 > column2)[0]
+
+        plt.axvline(x=indices1[0], color='green', linestyle='--')
+        plt.axvline(x=indices[0], color='blue', linestyle='--')
+        plt.axvline(x=1000, color='purple', linestyle='--')
+        plt.axvline(x=240, color='red', linestyle='-')
+
+        plt.text(500, 0.2, f'Left > Right: {indices[0]}\nNoOp > Right: {indices1[0]}\nCliff: {1000}\nStart Pos: 240',
+                 rotation=0, va='bottom')
+
+    if save_folder:
+        plt.savefig(os.path.join(save_folder, f'q-vals-{delta}-avg{len(seeds)}seeds-{linear}.png'))
+
+    plt.show()
+
+# TODO: ADD Functionality FOR GETTING MEAN BETA_MAX VALUE FOR EACH UPDATE OF ROBUST ESTIMATOR
+def get_and_plot_betas(delta, rolling_mean=10):
+    betas = np.load(os.path.join(root_folder,str(delta)) + '-betas.npy')
+
+    if rolling_mean:
+        betas = [np.mean(betas[i:i+rolling_mean]) for i in range(0,len(betas)-10)]
+    plt.plot(betas)
+    plt.title(f"Beta_max values Delta: {delta}")
+    plt.show()
+
+def load_and_test_agent(delta, test_games, render_games):
+    path = os.path.join(root_folder, str(delta)) + '-model'
+    env = SumoPPEnv()
+    # We define a barebones agent for the qualitaive test - Possible since select_action and test are never interfered with
+    agent = SumoAgent(env, replay_buffer=None, epsilon_decay=None, model_path=path)
+    test_results = agent.test(test_games=test_games, render_games=render_games)
+    return test_results
+
+def load_and_test_agent_cliff_car(delta, test_games, render_games):
+    path = os.path.join('cliff_car', 'test_results', 'Cliffcar-newoptim-linear-False-test_seed_6969_robust_factor_-1', '0.5-model')
+
+    env = CliffCar()
+    # We define a barebones agent for the qualitaive test - Possible since select_action and test are never interfered with
+    agent = CliffCarAgent(env, replay_buffer=None, epsilon_decay=None, model_path=path)
+    agent.test(test_games=test_games, render_games=render_games)
+
+
+def plot_average_of_seeds(seeds, delta_vals, linear=True, save_folder=None):
+
+    paths = [[f'sumo/test_results/newoptim-linear-{linear}-test_seed_{seed}_robust_factor_-1/{delta}-q_vals.npy'
+                     for seed in seeds] for delta in delta_vals]
+
+    for i in range(len(paths)):
+        q_vals = np.array([get_q_vals(path=path) for path in paths[i]])
+        plot_q_vals(np.mean(q_vals, axis=0), delta=delta_vals[i], vertical_lines=True,
+                    linear=linear, save_folder=save_folder)
+
+def plot_sar_stats(seeds, delta_vals, linear=True):
+
+    paths = [[f'sumo/test_results/newoptim-linear-{linear}-test_seed_{seed}_robust_factor_-1/{delta}-test_data.npy'
+                     for seed in seeds] for delta in delta_vals]
+
+    for i in range(len(paths)):
+        sar_data = np.array([np.load(path) for path in paths[i]])
+        # State Hist plotting
+        plt.hist(sar_data[:,:,:,0].flatten(), bins=100)
+        plt.title(f"States visited for delta value {delta_vals[i]}")
+        plt.show()
+
+        # Return Stats
+        returns = np.sum(sar_data[:,:,:,2],axis=2) # Returns for each game should be 1000 - 10 seeds, 100 games each
+        returns = returns[~np.isnan(returns)]# Remove NAN values - Don't know why they come...
+        mean_return, var_return = np.mean(returns), np.var(returns)
+        plt.hist(returns, bins=30)
+        plt.title(f"Episode Returns for delta value {delta_vals[i]}")
+        plt.axvline(x=mean_return, color='red', linestyle='-')
+        plt.show()
+        
+def plot_sumo_states(seeds, delta_vals, linear=True):
+    
+    paths = [[f'sumo/test_results/newoptim-linear-{linear}-test_seed_{seed}_robust_factor_-1/{delta}-test_data.npy'
+                     for seed in seeds] for delta in delta_vals]
+    
+    # Combine (not average) the states from all seeds
+    for i in range(len(paths)):
+        sar_data = np.array([np.load(path) for path in paths[i]])
+        sar_data = np.concatenate(sar_data, axis=0)
+        sar_data = sar_data[:,:,0].flatten()
+        
+        plt.hist(sar_data, bins=100)
+        plt.title(f"States visited for delta value {delta_vals[i]}")
+    plt.legend()
+    plt.show()
+
+# plot_sumo_states([6969, 4242, 6942, 123, 420, 5318008, 23, 22, 99, 10], [0.001, 0.005, 0.01], True)
+
+        # sar_data = np.mean(sar_data, axis=0) # Average over seeds
+
+# seeds = [6969, 4242, 6942, 123, 420, 5318008, 23, 22, 99, 10]
+# delta_vals = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 2, 3, 5]
+# save_folder = 'plots/q-vals'
+# plot_average_of_seeds(seeds, delta_vals, False, save_folder=save_folder)
+#
+# plot_sar_stats(seeds, delta_vals, True)
+
+# load_and_test_agent_cliff_car(0, 10, 10)
+# load_and_test_agent(0.01, 10, 10)
+#
+# paths_linear = [[f'sumo/test_results/newoptim-linear-True-test_seed_{seed}_robust_factor_-1/{delta}-q_vals.npy'
+#                 for seed in seeds] for delta in delta_vals]
+# paths_non_linear = [[f'sumo/test_results/newoptim-linear-False-test_seed_{seed}_robust_factor_-1/{delta}-q_vals.npy'
+#                 for seed in seeds] for delta in delta_vals]
+#
+# test_linear = [[f'sumo/test_results/newoptim-linear-True-test_seed_{seed}_robust_factor_-1/{delta}-test_data.npy'
+#                 for seed in seeds] for delta in delta_vals]
+#
+# for i in range(len(paths_linear)):
+#     q_vals = np.array([get_q_vals(path=path) for path in paths_linear[i]])
+#     plot_q_vals(np.mean(q_vals, axis=0), delta=delta_vals[i], vertical_lines=True)
+#
+
+#
+# # get_and_plot_q_vals(0.01, same_plot=True, vertical_lines=True)
+# q_vals = get_q_vals(0.05)
+# plot_q_vals(q_vals, 0.05, vertical_lines=True)
+get_and_plot_betas(0.01, rolling_mean=100)
+# get_and_plot_csv_data(delta=0.05)
+# # load_and_test_agent(delta=0.05, test_games=10, render_games=10)
+
+# for delta in [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 2, 3, 5]:
+#     get_and_plot_csv_data(delta)
+#     get_and_plot_q_vals(delta, same_plot=True, vertical_lines=True)
+#     get_and_plot_betas(delta, rolling_mean=100)
+#
+# # #
